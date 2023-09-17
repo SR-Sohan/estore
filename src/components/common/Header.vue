9a70b99d64c07e070b49b7534ccccd5e2f1bb05a
SR-Sohan <sr.sohanur22@gmail.com>
2023-09-17 23:19:39 +0600

    header change

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,13 +1,14 @@
 <script>
+import { RouterLink } from "vue-router";
 import logo from "../../assets/images/logo.svg";
 </script>
 
 <template>
   <header>
-    <div
-      class="top_header "
-    >
-      <div class="container d-flex align-items-center justify-content-between text-white">
+    <div class="top_header">
+      <div
+        class="container d-flex align-items-center justify-content-between text-white"
+      >
         <div>
           <i class="fa-solid fa-phone"></i>
           <a class="text-white" href="tel:+8801302460356">+8801302460356</a>
@@ -31,10 +32,10 @@ import logo from "../../assets/images/logo.svg";
     </div>
 
     <!--  NavBar -->
-    <nav class="navbar navbar-expand-lg ">
+    <nav class="navbar navbar-expand-lg">
       <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="@/assets/images/logo.svg" alt="">
+          <img src="@/assets/images/logo.svg" alt="" />
         </a>
         <button
           class="navbar-toggler"
@@ -50,27 +51,56 @@ import logo from "../../assets/images/logo.svg";
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link " aria-current="page" href="#">Home</a>
+              <router-link class="nav-link" to="/">Home</router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Products</a>
+              <router-link class="nav-link" to="products">Products</router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <router-link class="nav-link" to="about">About</router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact US</a>
+              <router-link class="nav-link" to="contact"
+                >Contact US</router-link
+              >
             </li>
-            
           </ul>
-          <div class="navbar_right">
+          <div class="navbar_right d-flex align-items-center">
             <div class="cart_section">
-                <a href="" class="cart_icon">
-                    <i class="fa-solid fa-basket-shopping"></i>
-                    <p>
-                        <span>08</span>
-                    </p>
-                </a>
+              <a href="" class="cart_icon">
+                <i class="fa-solid fa-basket-shopping"></i>
+                <p>
+                  <span>08</span>
+                </p>
+              </a>
+            </div>
+            <div class="auth ms-5">
+              <div class="sing_in">
+                <router-link to="/login">Login</router-link>
+              </div>
+              <!-- <div class="login_profile d-flex align-items-center">
+                <div class="user_profile_img">
+                  <img src="@/assets/images/user.jpg" alt="" />
+                </div>
+                <div id="dropDownUser" class="dropdown ms-1">
+                  <button
+                    class="btn  dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Sohanur
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <router-link class="dropdown-item" to="profile">Profile</router-link>
+                    </li>
+                    <li>
+                      <router-link class="dropdown-item" to="logout">Logout</router-link>
+                    </li>
+                  </ul>
+                </div>
+              </div> -->
             </div>
           </div>
         </div>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+   <div class="mx-auto w-50 my-5 shadow-lg p-4 bg-danger text-white rounded">
+        <h1>404</h1>
+        <h3>This page not Found</h3>
+   </div>
+</template>
